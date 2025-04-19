a2e3bd9fc3451b0fd15bd4f4f5fe0ee63bfe1f51
{your_name} <{your_email}>
2025-04-19 19:58:02 +0530

    profile update

--- components/landing-header.tsx
+++ components/landing-header.tsx
@@ -6,12 +6,16 @@ import Link from "next/link";
 import { ThemeToggle } from "./theme-toggle";
 import SignoutButton from "./auth/Signout";
 import { useUser } from "@/lib/stores/user";
+import { useEffect } from "react";
 
 export function LandingHeader() {
 
   // const { theme, setTheme } = useTheme();
-  const { userData, userLoading, updateUserData } = useUser();
-  console.log("userData", userData);
+  const { userData, setUserData } = useUser();
+  
+  useEffect(() => {
+      setUserData()
+    }, []);
 
   return (
     <header className="fixed top-0 left-0 w-full bg-background/80 backdrop-blur-md z-50 border-b border-border/40">
