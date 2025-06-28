ad67ff075e73ab52d120569462b4e2f784d862a9
Anirban Majumder <81350357+Anirban-Majumder@users.noreply.github.com>
2025-06-29 00:52:40 +0530

    fix magic & oauth

--- lib/functions/auth.ts
+++ lib/functions/auth.ts
@@ -6,10 +6,6 @@ export async function loginWithProvider(provider: 'google' | 'github') {
       provider,
       options: {
         redirectTo: `${window.location.origin}/auth/callback`,
-        queryParams: {
-          access_type: 'offline',
-          prompt: 'consent',
-        },
       },
     });
 
